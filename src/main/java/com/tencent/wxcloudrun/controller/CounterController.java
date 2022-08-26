a68585f182549b0acdb0bef42ab4931dd1a6c60b
yuanchangzhu <yuanchangzhu@pateo.com.cn>
2022-08-26 17:38:39 +0800

    add more gif

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -141,7 +141,7 @@ public class CounterController {
       if(content == null || "".equals(content)) return "";
 
       if(mImageKeywordList.contains(content)) {
-        return getImageReply(from,to,content);
+        return getImageReplyExtends(from,to,content);
       } else {
         return getTextReply(from, to,content);
       }
@@ -278,13 +278,16 @@ public class CounterController {
 
 
   private static final List<String> mImageKeywordList = new ArrayList<>();
-  private static final List<String> mChanMaoMediaIdList = new ArrayList<>();
-  private static final List<String> mXiaoZhuZhuMediaIdList = new ArrayList<>();
-  private static final List<String> mPikaMaoMediaIdList = new ArrayList<>();
-  private static final List<String> mDaPiMaoMediaIdList = new ArrayList<>();
-  private static final List<String> mXiaoMaomiMediaIdList = new ArrayList<>();
-  private static final List<String> mLanMaoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mChanMaoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mXiaoZhuZhuMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mPikaMaoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mDaPiMaoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mMaomaoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mLanMaoMediaIdList = new ArrayList<>();
   private static final List<ImageReplyBean> mDaChanZhuMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mDaLanZhuMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mMomoMediaIdList = new ArrayList<>();
+  private static final List<ImageReplyBean> mQinQinMediaIdList = new ArrayList<>();
 
   private static final String DA_PI_MAO = "大屁猫";
   private static final String CHAN_MAO = "馋猫";
@@ -293,8 +296,14 @@ public class CounterController {
   private static final String XIAO_LAN_MAO = "小懒猫";
   private static final String LAN_MAO = "懒猫";
   private static final String XIAO_ZHU_ZHU = "小猪猪";
-  private static final String XIAO_MAO_MI = "小猫咪";
+  private static final String ZHU_ZHU = "猪猪";
+  private static final String MAO_MAO = "猫猫";
   private static final String DA_CHAN_ZHU = "大馋猪";
+  private static final String CHAN_ZHU = "馋猪";
+  private static final String DA_LAN_ZHU = "大懒猪";
+  private static final String LAN_ZHU = "懒猪";
+  private static final String MO_MO = "摸摸";
+  private static final String QIN_QIN = "亲亲";
 
   static {
     mImageKeywordList.add(DA_PI_MAO);
@@ -303,90 +312,89 @@ public class CounterController {
     mImageKeywordList.add(PI_KA_MAO);
     mImageKeywordList.add(XIAO_LAN_MAO);
     mImageKeywordList.add(LAN_MAO);    
-    mImageKeywordList.add(XIAO_ZHU_ZHU);  
-    mImageKeywordList.add(XIAO_MAO_MI);   
-    mImageKeywordList.add(DA_CHAN_ZHU);    
+    mImageKeywordList.add(XIAO_ZHU_ZHU); 
+    mImageKeywordList.add(ZHU_ZHU);  
+    mImageKeywordList.add(MAO_MAO);   
+    mImageKeywordList.add(DA_CHAN_ZHU);  
+    mImageKeywordList.add(CHAN_ZHU);  
+    mImageKeywordList.add(DA_LAN_ZHU);  
+    mImageKeywordList.add(LAN_ZHU); 
+    mImageKeywordList.add(MO_MO); 
+    mImageKeywordList.add(QIN_QIN);    
 
     //馋猫
-    mChanMaoMediaIdList.add("og9uZrJ4VLOt3kLhu_AuIF0FnayrYoQ5aF0UT1EL-YP3Bg2fPXVK9TQTyinnXeDc");
-    mChanMaoMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPnHqnpPG75t3JD2GHHQvdat52rVgZJOa_AMb_UdLfo4Y");
-    mChanMaoMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPtJ7fqM5EQxVlH2quhdIWUFJRN0J2Ka59kdky2LDRavA");
-    mChanMaoMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPkq7-E0g04TMAguaEYNSkN-Pe0cNPJz6tBsrp5iO_OQS");
-    mChanMaoMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPvrEkZK-9NM3IQt-AAK8-ppzbBYg12ma2_C44KIhAYfm");
+    mChanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMV7Yibh9z98yr7nkwaNKgzYS9xA6mmB4HnTADAJk0JDtGicprJHY4yxPQ/0?wx_fmt=gif","好好吃"));
+    mChanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMWdSFF0rbTnxWwRgP4HwmsW3hK3hkBvNayJibE6GGW2zbpcSAgMic319g/0?wx_fmt=gif","好好吃"));
+    mChanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMwVLoSOKB8QxSIsTIe3lQLFQxk3qNRNxKzN6sr7S1iaCBicggd2icHvmfA/0?wx_fmt=gif","好好吃"));
+    mChanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMROpOMkqibJBvtqq156D2nazUk4WFz74Zo64icwFq2BBjTCIHuTQONClQ/0?wx_fmt=gif","好好吃"));
+    mChanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMNG75HeVIqj9sbtTY5r340HusTBTCnLAGSkz7aak8N07kGqf7gtcgNw/0?wx_fmt=gif","好好吃"));
 
     //小猪猪
-    mXiaoZhuZhuMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPi7lyHJOutaX70fdRSd7UwMBgEvuEJaD6BQvE1Upq-tR");
-    mXiaoZhuZhuMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPt6o_7YUhJzhIvX3qHKq-LFN_MnmZf_1rWMfULAL4ouY");
-    mXiaoZhuZhuMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPlLjuJWmnRMDFLQEMsg4QklKsGfPYF2b9I7pQcs4ynLH");
+    mXiaoZhuZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMogKJ5AsTJDumicLib3ddRJSywhfuMlvDHEGlDk9YoU8qVXsf7RzH5ia5w/0?wx_fmt=gif","哼哼哼哼"));
+    mXiaoZhuZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMLSoiaYia2ZfC6KlfIDZVicGk2QBrLfewj4NO7JWVkX9bP5oqcXIwe2SZA/0?wx_fmt=gif","哼哼哼哼"));
+    mXiaoZhuZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMv3ravQWtls7iceHqibhb43wYzwiabbic6JZ7oyagTSKdhBxLMyWiczHoEJw/0?wx_fmt=gif","哼哼哼哼"));
+    mXiaoZhuZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMhDg6hoXGnFvBOyHMZUJ0fIibuFSLvUqZAOxmLzokpLqVjRseYiaPsyqw/0?wx_fmt=gif","哼哼哼哼"));
+    mXiaoZhuZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMMS4LqgddKbXiccFZPxyygAibGotGiafG3sx1ohGl9IR44S7ibvxc9XnsTw/0?wx_fmt=gif","哼哼哼哼"));
+
 
     //皮卡猫
-    mPikaMaoMediaIdList.add("Uqr_FN07KQF_j-eLL1GCno8Ifulw8wybn1ZFts0A-ofOuXirx-gon5F7izi-BywM");
+    mPikaMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUx3o11sjNzOGh18uQYgbQtYo3ibCVaSDUOcrRHK8uIiaGTElrqOcVQiarwnCApzbEMmSffaDm4yHUwQ/0?wx_fmt=png", "皮卡皮卡喵～"));
 
 
     //大屁猫
-    mDaPiMaoMediaIdList.add("og9uZrJ4VLOt3kLhu_AuIDgU4_HTAnlBVDmIJXiXtaiEDlLrt7ZkpXnxN6l432YF");
+    mDaPiMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM4EDdeAibltYABobWcHWssUNSEfwCNkiaXqTBN8sicd14WZwyMNuY3rF0g/0?wx_fmt=png","大屁猫，猫屁大～"));
+    mDaPiMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMiaMY6vEwTg2iaiaR1zicBoibQgVOBYicyXMjZCW5VehITFhxqeeFe81rK4SQ/0?wx_fmt=png","大屁猫，猫屁大～"));
+    mDaPiMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMMWOqcb6PmkHdo82x3iaVM9uhPsyVWoh98ABpJY66ALvX1icibRzTRslrw/0?wx_fmt=png","大屁猫，猫屁大～"));
 
-    //小猫咪
-    mXiaoMaomiMediaIdList.add("Fqbf5z1cZh_UYBc_M1YfPnQe8yQo5lJ5ismgXy8OsQKa4sEWVpPo0NPFcQLF_J-E");
-    mXiaoMaomiMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_k6ROzq-WulBqxfXR8vswJILz3a8lHhj5y6tSAxqkC05M");
-    mXiaoMaomiMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_k7bCf9vMU-9D_gUPjLHL0sH19-PxVMoYRp8YC4jynrXo");
-    mXiaoMaomiMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_kx82fCOq9hIwogPS4-oRQboYzRYZu6z5JXqSJ_fYzCX");
-    mXiaoMaomiMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_k9uAGH2gA6MrGWyqXWjHlY6726k4rECZ01vtSeWet217");
+
+    //猫猫
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMlt4xs6ZYetAmExqx6nibbfs5ib5C1Kdibm0n1I0KnUq8Iic7L9noSGYiaUA/0?wx_fmt=gif","喵喵喵喵"));
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMNpEbZGStTSdnKF18wKnt3iatl7hhaH1u0jk8WGy2Djx6u0orPbtwvUA/0?wx_fmt=gif","喵喵喵喵"));
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM4thvT4KgcmlyALh12PNWY6jK8yXWWqlMib5OYax2Q9HH8JzVJzn4cqQ/0?wx_fmt=gif","喵喵喵喵"));
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMoTia0pY3yX3X1OcTzv5LLrFH8IJ1vrM7icCzCqfNJTgiaHgBeS2jibK5pg/0?wx_fmt=gif","喵喵喵喵"));
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM8w6xNdd789nhTMtDicd9kiaPKNKJFxmzCKUhD6BUhJNfwCKNZNVxNrzg/0?wx_fmt=gif","喵喵喵喵"));
+    mMaomaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM7fqqeA6ysIkbogooPVKAgf0ibib5KhypSdfetPlVvmKlrK1ic6fj6ZgfA/0?wx_fmt=gif","喵喵喵喵"));
+
+    
 
     //懒猫
-    mLanMaoMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_k3Y24EQudy0AyxVIMaGAqJigDCAtGHBXCX48nIHKUtt_");
-    mLanMaoMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_k-cuLFmTAxAXMwAX57HVOcFXwOpRigqwesQVfvnQkMWC");
-    mLanMaoMediaIdList.add("odIJHwiJ_nC4Sfu-aEU_kxROYlrxfMxEsbne9Udh4XcGSodXZhhI0AHSkxwG4hN_");
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMxa856ibdcQM3J1zvl8Qfiak59CPEV9ZMwbrfFuNnoMNnqrUjfHqOtibJQ/0?wx_fmt=gif","呼呼呼"));
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMYYoWY3YJuwQb0xdR41txmITANrhK74EK5smDT2TEkRgEtBQBvXYQeg/0?wx_fmt=gif","呼呼呼"));
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMiaibuRBnaQfSoCAkAlyrVNndGJVXt2ycqHA99y1EoeKOlLWgNvqZedOw/0?wx_fmt=gif","呼呼呼"));
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMtwFPHlTprAWhFiaribnibbkLkJhldqo1y94E5oib1etVM4sAYt8EhoF6rQ/0?wx_fmt=gif","呼呼呼"));
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM34oxgPiaphnxs8FCSwvcJ3Zv4GIfJGDCGtdHNq9frIR5aFCsPt0HjIg/0?wx_fmt=gif","呼呼呼"));
+    mLanMaoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuM8RBY8uJd3JUwp6Gn5yMfPnNJeSEEOFvbjO2icZRyIwMU52aH75zhh4g/0?wx_fmt=gif","呼呼呼"));
+
 
     //大馋猪
     mDaChanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUx3o11sjNzOGh18uQYgbQtLy8CBPoufaNCcYCYnjNyNQLz5pboIxYribQAxgGloqvbGBP5ic4GFONQ/0?wx_fmt=gif","好好吃"));
+    mDaChanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMEh7NVYh1lw4pPmx34wGSaKLeiaJ4XRGjbKAL9xlwibghC75na0kHJ2gA/0?wx_fmt=gif","好好吃"));
+    mDaChanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMA9Xjln6rB1QL3QHMZibB12HDJSRgOJlmr2K9ZIeuIUQpVC6M0qmWEHQ/0?wx_fmt=gif","好好吃"));
+    mDaChanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMiaMg8NI8KHmWUxDiawpp7hy17G4lhpBmIw8utgnC23WVboI5s9ZCBAlA/0?wx_fmt=gif","好好吃"));
+    mDaChanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMkcIzYtWZHyXBBok4fCApeTNI8xTP42ome5iaPfGgAVVEUhTt0GAKNPQ/0?wx_fmt=gif","好好吃"));
+
+    //懒猪
+    mDaLanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMkE17TObfoBOibP6Xd4vbQqJL3cmu1tS30RnD375Bxiavyibh3PCZuuBaQ/0?wx_fmt=gif", "呼呼呼"));
+    mDaLanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMhedS5ZCVnG6yZk5AZibyrq9uZcpicY3VNEGqibaUKOXnpFZu9RSI6HpbA/0?wx_fmt=gif", "呼呼呼"));
+    mDaLanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMgU8DS1zF6ibhMPLuBTibJMVgSAVCMBVibk2IlvkjYfu063icL5xBRTDXEA/0?wx_fmt=gif", "呼呼呼"));
+    mDaLanZhuMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMXF1C7cRneKRb89USm1fMRODveh7k77tSicOrf19P78ibH5UgKSp3Qa4w/0?wx_fmt=gif", "呼呼呼"));
+
+    //摸摸
+    mMomoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMiaD0XRKRPuicIBpzlpwEaic0915eTT5AG0PSTKFGibA7Fxhsee0ASibN1Qg/0?wx_fmt=gif", "好舒服啊"));
+    mMomoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMCAeqUzysrNOQVTSOctTQl9dQF60LWJs8832CK2QQv1Mj88vpljNH2Q/0?wx_fmt=gif", "好舒服啊"));
+    mMomoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_png/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMmibuGAIQhYvl7kNPpG1DNnlibt7CXeWmaUiaDkue1WPFIzqJuEwSn6ictg/0?wx_fmt=gif", "好舒服啊"));
+    mMomoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMdyhatP9fh0Dn8fY9AXMXJer5UrLAJfN0axYRjL2Bn9r4OeZz77qX6g/0?wx_fmt=gif", "好舒服啊"));
+    mMomoMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMIvor6emUXdqNtMJqyNCWeh0nibaicZdfKtDjichKFaXVPeI6XtRHJfZzg/0?wx_fmt=gif", "好舒服啊"));
+
+    //亲亲
+    mQinQinMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMf97ejwz1UtsQkj3QuJ0E45rX9FUTXJ9MsjoUKPxDV3pvPNQaBaaE3A/0?wx_fmt=gif", "mua~"));
+    mQinQinMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMAxibhN0ibnXvtl1riamu46ZctnGQ5uWsGy7kEtPxWScP4UvP8F7jBf9JQ/0?wx_fmt=gif", "mua~"));
+    mQinQinMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMHRduoYGRicqnkTW1iaibQbia0Wp1iaTVKKRsaG7kGcIcuUQfKPZIgPomYHw/0?wx_fmt=gif", "mua~"));
+    mQinQinMediaIdList.add(new ImageReplyBean("http://mmbiz.qpic.cn/sz_mmbiz_gif/t4K8ARPcgicUtbQ8mHFhM5uqyPjv0PZuMq9alicoVN4ZnpFYxfF0ibfeQRicicQGTzJ26eY5XuiaMpntia5RQkRjL2SHA/0?wx_fmt=gif", "mua~"));
+
+
   }
 
-  private String getImageReply(String from,String to,String content) {
-    String mediaId = "";
-    switch(content) {
-      case DA_PI_MAO:
-      mediaId = getRandomMediaId(mDaPiMaoMediaIdList);
-        break;
-      case LAN_MAO:
-      case XIAO_LAN_MAO:
-        mediaId = getRandomMediaId(mLanMaoMediaIdList);
-        break;
-      case CHAN_MAO:
-      case XIAO_CHAN_MAO:
-        mediaId = getRandomMediaId(mChanMaoMediaIdList);
-        break;
-      case PI_KA_MAO:
-        mediaId = getRandomMediaId(mPikaMaoMediaIdList);     
-        break;
-      case XIAO_ZHU_ZHU :
-        mediaId = getRandomMediaId(mXiaoZhuZhuMediaIdList);
-        break;
-      case XIAO_MAO_MI :
-        mediaId = getRandomMediaId(mXiaoMaomiMediaIdList);
-        break;
-      case DA_CHAN_ZHU:
-        return getImageReplyExtends(from,to,content);
-      default:
-        return "";                  
-    }
-    JSONObject reply = new JSONObject();
-    reply.put(FROM_USER_NAME, to);
-    reply.put(TO_USER_NAME, from);
-    reply.put(TIME, System.currentTimeMillis() / 1000);
-    reply.put(TYPE, "image");
-    JSONObject image = new JSONObject();
-    image.put(MEDIA_ID, mediaId);
-    reply.put(IMAGE, image);
-    logger.info("reply " + reply);
-    return reply.toString();
-  }
-
-  private String getRandomMediaId(List<String> list) {
-    Random random = new Random();
-    int index = random.nextInt(list.size());
-    return list.get(index);
-  }
 
   /**
    * 
@@ -413,9 +421,40 @@ public class CounterController {
     ImageReplyBean imageReplyBean = null;
     switch(content) {
       case DA_CHAN_ZHU:
-      imageReplyBean = getRandomImageReplyBean(mDaChanZhuMediaIdList);
+      case CHAN_ZHU:
+        imageReplyBean = getRandomImageReplyBean(mDaChanZhuMediaIdList);
         break;
-
+      case DA_LAN_ZHU:
+      case LAN_ZHU:
+        imageReplyBean = getRandomImageReplyBean(mDaLanZhuMediaIdList);      
+        break;
+      case ZHU_ZHU:
+      case XIAO_ZHU_ZHU:
+        imageReplyBean = getRandomImageReplyBean(mXiaoZhuZhuMediaIdList);      
+        break;     
+      case PI_KA_MAO:
+        imageReplyBean = getRandomImageReplyBean(mPikaMaoMediaIdList);      
+        break;   
+      case MAO_MAO:
+        imageReplyBean = getRandomImageReplyBean(mMaomaoMediaIdList);      
+        break; 
+      case DA_PI_MAO:
+        imageReplyBean = getRandomImageReplyBean(mDaPiMaoMediaIdList);      
+        break;   
+      case LAN_MAO:
+      case XIAO_LAN_MAO:
+        imageReplyBean = getRandomImageReplyBean(mLanMaoMediaIdList);      
+        break;  
+      case CHAN_MAO:
+      case XIAO_CHAN_MAO:
+          imageReplyBean = getRandomImageReplyBean(mChanMaoMediaIdList);      
+          break;   
+      case MO_MO:
+          imageReplyBean = getRandomImageReplyBean(mMomoMediaIdList);      
+          break;         
+      case QIN_QIN:
+          imageReplyBean = getRandomImageReplyBean(mQinQinMediaIdList);      
+          break;                        
       default:
         return "";                  
     }
